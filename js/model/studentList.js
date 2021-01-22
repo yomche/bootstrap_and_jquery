e3184eaf0d679e16ce4886ab471f07725a1dec51
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-01-22 17:20:19 +0300

    create student list

---
+++ js/model/studentList.js
@@ -0,0 +1,12 @@
+var studentList = new Map( [      
+["c93fd420-8833-4c77-ac37-e3ed0cabdf1d", "Kenneth Lam"],
+["6a714dd0-0710-45a7-9594-8b6384817ab1", "Christian Boyd"],
+["5c8b9673-a921-4688-98aa-703a64aa8fb8", "Judith Fletcher"],
+["d16eb71d-0f86-4213-87a6-dc343db23e32", "Elsa Boyd"],
+["3933951e-ac6f-44dd-b63b-68532093e152", "Charles Allen"],
+["b836a03f-5521-49ce-acc8-2ec02e53b60b", "Sarah Williams"],
+["80374878-1736-4427-9b8c-81253857e35f", "Stephanie Weber"],
+["d4cc7808-d14a-472b-9356-f180851ba00d", "Gabriel Lake"],
+["874ee5cf-5652-47b0-a8fa-b7c5bef77237", "Lawrence Carlson"],
+["68276f5f-7d9d-4de6-9bd1-bf74593b0596", "Stephen Kaufman"]
+]);
